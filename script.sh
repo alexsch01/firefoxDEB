8d591efa1062865fa7d280d965036877908c90f9
Alex Schwartz <alexschwartz01@gmail.com>
2024-01-25 11:31:55 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+wget -qO - https://packages.mozilla.org/apt/repo-signing-key.gpg | gpg --dearmor | sudo dd of=/etc/apt/keyrings/packages.mozilla.org.gpg
+echo 'deb [ signed-by=/etc/apt/keyrings/packages.mozilla.org.gpg ] https://packages.mozilla.org/apt mozilla main' | sudo tee /etc/apt/sources.list.d/mozilla.list
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt update
+sudo apt install firefox -y
