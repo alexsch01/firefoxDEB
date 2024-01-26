63bf6438942f6349076f7ba3a1ddebcd5dc8a6de
Alex Schwartz <alexschwartz01@gmail.com>
2024-01-26 07:11:49 -0500

    only install on x86_64

--- script.sh
+++ script.sh
@@ -1,11 +1,16 @@
-wget -qO - https://packages.mozilla.org/apt/repo-signing-key.gpg | gpg --dearmor | sudo dd of=/etc/apt/keyrings/packages.mozilla.org.gpg
-echo 'deb [ signed-by=/etc/apt/keyrings/packages.mozilla.org.gpg ] https://packages.mozilla.org/apt mozilla main' | sudo tee /etc/apt/sources.list.d/mozilla.list
-
-echo '
-Package: *
-Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla
-
-sudo apt update
-sudo apt install firefox -y
+if [ $(arch) = 'x86_64' ]
+then
+  wget -qO - https://packages.mozilla.org/apt/repo-signing-key.gpg | gpg --dearmor | sudo dd of=/etc/apt/keyrings/packages.mozilla.org.gpg
+  echo 'deb [ signed-by=/etc/apt/keyrings/packages.mozilla.org.gpg ] https://packages.mozilla.org/apt mozilla main' | sudo tee /etc/apt/sources.list.d/mozilla.list
+  
+  echo '
+  Package: *
+  Pin: origin packages.mozilla.org
+  Pin-Priority: 1000
+  ' | sudo tee /etc/apt/preferences.d/mozilla
+  
+  sudo apt update
+  sudo apt install firefox -y
+else
+  echo 'Architecture not x86_64, install skipped'
+fi
